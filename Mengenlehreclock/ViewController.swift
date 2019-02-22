88a07b77138cffff24868fe905bc82d4d18f35d9
Karl Sigiscar <ksigiscar@bink.com>
2019-02-22 08:32:53 +0000

    Initial Commit

---
+++ Mengenlehreclock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mengenlehreclock
+//
+//  Created by Karl Sigiscar on 22/02/2019.
+//  Copyright © 2019 ObjectMentor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
